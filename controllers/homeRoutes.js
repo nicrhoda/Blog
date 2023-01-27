0755c1349fc4ffd3a343938ffca96122974af477
nic <nrhoda14@gmail.com>
2023-01-26 23:30:50 -0600

    routed login page in homeroutes, created login.js to handle login form, began work on displaying all posts on homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,9 +3,23 @@ const { User, Post, Comments } = require('../models');
 const sequelize = require('../config/connection');
 const { Model } = require('sequelize');
 
-
 router.get('/', (req, res) => {
-    res.render('homepage');
+    Post.findAll({
+        Include: [User],
+    }).then((data) => {
+        const postData = data.map((post) => post.get({ plain: true }));
+        res.render('homepage', { postData });
+    }).catch((err) => {
+        res.status(500).json(err);
+    });
+});
+
+router.get('/login', (req, res) => {
+    if(req.session.logged_in) {
+        res.redirect('homepage');
+        return;
+    }
+    res.render('login');
 });
 
 
---
+++ public/js/login.js
@@ -0,0 +1,29 @@
+//login handler
+
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+    
+    const username = document.querySelector('#username-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if(username && password) {
+        const response = await fetch('/api/user/login', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+        if(response.ok) {
+            document.location.replace('/homepage');
+        } else {
+            document.getElementById('formValidation').innerHTML =
+            `<div class="alert" role="alert">Incorrect username or password</div>`
+        }
+    }
+    if(username === "" || password === "") {
+        document.getElementById('formValidation').innerHTML =
+        `<div class="alert" role="alert">username and password required</div>`
+    }
+};
+
+const loginSubmit = document.getElementById('login-form');
+loginSubmit.addEventListener('submit', loginFormHandler);
