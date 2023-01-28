d92ffdc331ee6339a4610ca36470d136b1cdc70e
nic <nrhoda14@gmail.com>
2023-01-27 22:56:53 -0600

    added and fixed logout routes and logic

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -45,5 +45,13 @@ router.post('/signup', async (req, res) => {
 });
 
 //logout route
-
+router.post('/logout', (req, res) => {
+    if(req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
 module.exports = router;
---
+++ public/js/logout.js
@@ -0,0 +1,15 @@
+//logout handler
+
+async function logout() {
+    const response = await fetch('/api/user/logout', {
+        method:'POST',
+        headers: { 'Content-Type': 'application/json' },
+    });
+    if(response.ok) {
+        document.location.replace('/');
+    } else {
+        alert(response.statusText);
+    }
+};
+
+document.getElementById('logout').addEventListener('click', logout);
